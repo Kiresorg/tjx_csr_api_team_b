cd7f2fb2d10aae2d63b375897fa4b44df4d569b6
Lavon Burgo <Lavon-TJX@tjx.com>
2021-03-16 16:22:56 -0400

    fleshed out the orders MVC

---
+++ app/models/orders.model.js
@@ -0,0 +1,25 @@
+const { mongo } = require("mongoose");
+const { mongoose } = require(".");
+
+module.exports = mongoose => {
+    var schema = mongoose.Schema(
+        {
+            first_name: String,
+            customer_id: Number,
+            order_status_code: String,
+            datetime_order_placed: Date,
+            total_order_price: Number,
+            order_notes: String
+        },
+        { timestamps: true }
+    );
+
+    schema.method("toJSON", function() {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+
+    const Order = mongoose.model("order", schema);
+    return Order;
+};
---
+++ app/routers/orders.route.js
@@ -0,0 +1,27 @@
+module.exports = app => {
+    const controller = require('../controllers/orders.controller');
+
+    var router = require("express").Router();
+
+    // Endpoint: GET on api/orders/:id 
+    // Get one order
+    router.get('/:id', controller.getOne);
+
+    // Endpoint: GET on api/orders/ 
+    // Get all orders
+    router.get('/', controller.getAll);
+
+    // Endpoint: PUT on api/orders/:id
+    // Edit an order
+    router.put('/:id', controller.editOrder)
+
+    // Endpoint: POST on api/orders
+    // Create an order
+    router.post('/', controller.createOrder);
+
+    // Endpoint: DELETE on api/orders/:id
+    // Delete an order
+    router.delete('/:id', controller.deleteOrder);
+
+    app.use("/api/orders", router);
+};
